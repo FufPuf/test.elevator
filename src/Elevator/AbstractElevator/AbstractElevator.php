af519c69deb471f7becf058b3ff1b473fe796216
mykola.petrov mykola.petrov@vaimo.com <mykola.petrov@vaimo.com>
2021-01-20 11:37:23 +0200

    empty elevator work

--- src/Elevator/AbstractElevator/AbstractElevator.php
+++ src/Elevator/AbstractElevator/AbstractElevator.php
@@ -1,37 +1,37 @@
 <?php
 
 require_once "/Users/nikolaypetrov/Vaimo/test/test.elevator/src/Elevator/AbstractElevator/ElevatorInterface/ElevatorInterface.php";
-
+require_once "/Users/nikolaypetrov/Vaimo/test/test.elevator/src/Passenger/PassengerInterface/PassengerInterface.php";
 
 abstract class AbstractElevator implements ElevatorInterface
 {
 
-    abstract public function __construct(Passenger $passenger);
+    abstract public function __construct(PassengerInterface $passenger);
 
     abstract public function move();
 
-    protected function elevatorStop()
+    protected function elevatorStop($floor)
     {
-        echo "Elevator stop";
+        echo "Elevator stop on " . $floor . "</br>";
     }
 
-    protected function openDoor()
+    protected function openDoor($floor)
     {
-        echo "Open door";
+        echo "Open door on " . $floor . "</br>";
     }
 
-    protected function closeDoor()
+    protected function closeDoor($floor)
     {
-        echo "Close door";
+        echo "Close door on " . $floor . "</br>";
     }
 
-    protected function moveUp()
+    protected function moveUp($floor)
     {
-        echo "Move Up";
+        echo "Move Up to " . $floor . "</br>";
     }
 
-    protected function moveDown()
+    protected function moveDown($floor)
     {
-        echo "Move Down";
+        echo "Move Down to " . $floor . "</br>";
     }
 }
--- src/Elevator/Elevator.php
+++ src/Elevator/Elevator.php
@@ -3,6 +3,7 @@
 require_once "/Users/nikolaypetrov/Vaimo/test/test.elevator/src/Passenger/PassengerInterface/PassengerInterface.php";
 require_once "/Users/nikolaypetrov/Vaimo/test/test.elevator/src/Elevator/AbstractElevator/AbstractElevator.php";
 require_once "/Users/nikolaypetrov/Vaimo/test/test.elevator/src/Elevator/AbstractElevator/ElevatorInterface/ElevatorInterface.php";
+require_once "/Users/nikolaypetrov/Vaimo/test/test.elevator/src/Passenger/Passenger.php";
 
 class Elevator extends AbstractElevator
 {
@@ -11,35 +12,60 @@ class Elevator extends AbstractElevator
     private static $enteredFloors = [];
     private static $requiredFloors = [];
     private static $passengersWeight = [];
-    private static $elevatorStatus = [];
+    public static $elevatorStatus = [];
     private $maxWeight = 600;
 
     public function __construct(PassengerInterface $passenger)
     {
         $id = $passenger->getID();
-        self::$passengersId = $id;
+        self::$passengersId[] = $id;
         self::$enteredFloors[$id] = $passenger->getEnteredFloor();
         self::$requiredFloors[$id] = $passenger->getRequiredFloor();
         self::$passengersWeight[$id] = $passenger->getPassengerWeight();
+        return $this;
     }
 
     public function move()
     {
+        if(empty(self::$elevatorStatus)) {
+            $this->ifEmpty();
+        }
+
+        if(!empty(self::$elevatorStatus)) {
+
+        }
+    }
+
+    private function ifEmpty()
+    {
         $closer = $this->checkCloser(self::$enteredFloors);
+
         if($this->checkWeight()) {
+
             if($closer > self::$elevatorLocation) {
-                $this->moveUp();
-                while ($closer == self::$elevatorLocation) {
-                    self::$elevatorLocation++;
-                }
+                $this->moveUp($closer);
+                $this->elevatorStop($closer);
+                self::$elevatorLocation = $closer;
+                $this->openDoor(self::$elevatorLocation);
+                $this->pickUp();
+                $this->closeDoor(self::$elevatorLocation);
             }
-            if($closer > self::$elevatorLocation) {
-                $this->moveDown();
-                while ($closer == self::$elevatorLocation) {
-                    self::$elevatorLocation--;
-                }
+
+            if($closer < self::$elevatorLocation) {
+
+                $this->moveUp($closer);
+                $this->elevatorStop($closer);
+                self::$elevatorLocation = $closer;
+                $this->openDoor(self::$elevatorLocation);
+                $this->pickUp();
+                $this->closeDoor(self::$elevatorLocation);
             }
         }
+    }
+
+    private function ifNoEmpty()
+    {
+
     }
 
     private function pickUp()
@@ -50,13 +76,13 @@ class Elevator extends AbstractElevator
             unset(self::$enteredFloors[$this->checkRequired()]);
             unset(self::$requiredFloors[$this->checkRequired()]);
             unset(self::$passengersWeight[$this->checkRequired()]);
+            echo "Passenger entered </br>";
         }
     }
 
     private function dropOff()
     {
         if($this->checkRequired()) {
-            unset(self::$elevatorStatus[$this->checkRequired()]);
             unset(self::$elevatorStatus[$this->checkRequired()]);
         }
     }
@@ -111,4 +137,7 @@ class Elevator extends AbstractElevator
 
 }
 
-$elev = new Elevator(new Passenger(1, 2, 80));
+$elev = new Elevator(new Passenger(3, 2, 80));
+$elev = new Elevator(new Passenger(2, 5, 80));
+$elev->move();
+var_dump(Elevator::$elevatorStatus);
